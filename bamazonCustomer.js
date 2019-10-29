2ac7878cbccc06c63ff3b2a146a38c4baaa17e3f
Manuel R <mannyvazqsan1@icloud.com>
2019-10-28 23:46:25 -0400

    added inquirer prompt for purchase

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -18,8 +18,9 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   showInventory();
-  runSearch();
 });
+
+
 function showInventory() {
     var query = "SELECT item_id, product_name, price_dollars FROM products";
     connection.query(query, function(err,res) {
@@ -27,45 +28,32 @@ function showInventory() {
         for (var i = 0; i < res.length; i++) {
             console.log(res[i]);
         }
-        runSearch();
     })
 }
 
-function runSearch() {
+function productSearch() {
   inquirer
-    .prompt({
-      name: "action",
-      type: "rawlist",
-      message: "What would you like to do?",
-      choices: [
-        "Show inventory",
-        "Find all artists who appear more than once",
-        "Find data within a specific range",
-        "Search for a specific song",
-        "Find artists with a top song and top album in the same year"
-      ]
-    })
-    .then(function(answer) {
-      switch (answer.action) {
-      case "Find songs by artist":
-        artistSearch();
-        break;
-
-      case "Find all artists who appear more than once":
-        multiSearch();
-        break;
-
-      case "Find data within a specific range":
-        rangeSearch();
-        break;
-
-      case "Search for a specific song":
-        songSearch();
-        break;
-
-      case "Find artists with a top song and top album in the same year":
-        songAndAlbumSearch();
-        break;
+    .prompt([
+      {
+      name: "itemID",
+      type: "input",
+      message: "Enter desired product ItemID",
+      validate: function(value) {
+        if (isNaN(value) === false) {
+          return true;
+        }
+        return false;
       }
-    });
-}
+    },
+    {
+      name: "quantityDesired",
+      type: "input",
+      message: "How many of this item would you like to purchase?"
+    }
+  ])
+  .then(answer => {
+    item_id = answer.item_id;
+    qtyPurchase = answer.quantityDesired;
+  });
+ }
+productSearch();
